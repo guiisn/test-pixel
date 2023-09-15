141b283fb8d6b069653245fa53b30287310df196
guiisn <glhrmsouzanunes@gmail.com>
2023-09-15 18:53:22 -0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { UserConfigExport, defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
+import typescript from '@rollup/plugin-typescript';
 
 // https://vitejs.dev/config/
 export default ({ mode }): UserConfigExport => {
@@ -9,11 +12,35 @@ export default ({ mode }): UserConfigExport => {
 
   return defineConfig({
     build: {
-      sourcemap: true,
+      lib: {
+        entry: path.resolve(__dirname, 'src/lib/index.ts'),
+        name: 'pixel',
+        fileName: 'pixel',
+      },
+      rollupOptions: {
+        external: ['react', 'react-dom'],
+        output: {
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+          },
+        },
+      },
     },
     plugins: [
       react(),
+      dts(),
       tsconfigPaths(),
+      typescript(
+        {
+          target: 'es2022',
+          rootDir: './src',
+          declaration: true,
+          declarationDir: './dist',
+          exclude: './node_modules/**',
+          allowSyntheticDefaultImports: true,
+        },
+      ),
     ],
     test: {
       globals: true,
