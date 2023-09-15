1c3099ecd49e393562ef6902754425b50dbc3f1c
guiisn <glhrmsouzanunes@gmail.com>
2023-09-15 18:49:49 -0300

    fix: change export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,13 @@
-export * from './components/Buttons';
+import ActionButton from './components/Buttons/ActionButton';
+import Button from './components/Buttons/Button';
+import HeaderReturnButton from './components/Buttons/HeaderReturnButton';
+import IconButton from './components/Buttons/IconButton';
+import PanelButton from './components/Buttons/Panel';
+
+export {
+  ActionButton,
+  Button,
+  HeaderReturnButton,
+  IconButton,
+  PanelButton,
+};
